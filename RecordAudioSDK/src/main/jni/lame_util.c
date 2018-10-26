a949d904f90789bce82c70e4a509e775a2e6683a
yiheng <dgzx106@163.com>
2018-10-26 18:11:54 +0800

    cmake编译mp3lame

--- RecordAudioSDK/src/main/jni/lame_util.c
+++ RecordAudioSDK/src/main/jni/lame_util.c
@@ -1,4 +1,4 @@
-#include "libmp3lame-3.100/include/lame.h"
+#include "mp3lame-3.99.5/include/lame.h"
 #include "com_hardrubic_sdk_recordaudio_record_LameUtil.h"
 #include <stdio.h>
 #include <jni.h>
